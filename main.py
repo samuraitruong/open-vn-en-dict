d3883c1513f34fce78a5fc80740db532ff34048a
Truong Nguyen <truong.nguyen@team.telstra.com>
2019-11-12 14:41:54 +1100

    update json

--- main.py
+++ main.py
@@ -117,9 +117,9 @@ def getWord(word):
 if len(sys.argv) > 1:
     pprint.pprint(getWordFromLaban(sys.argv[1]))
     exit()
-print ("Retrive list of all english words")
+print("Retrive list of all english words")
 words = getWordList()
-print("Total english words")
+print("Total english words %d".format(len(words)))
 with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
     future_to_url = {executor.submit(
         getWordFromLaban, key): key for key in words}
